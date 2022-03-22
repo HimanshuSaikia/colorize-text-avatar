1e64468a40984f1977b4e6c31aaa806a0b7d76b0
HimanshuSaikia <42633708+HimanshuSaikia@users.noreply.github.com>
2022-03-22 16:56:32 +0530

    Update colorized_text_avatar.dart
    User set Background color will not take effect unless you replace
    backgroundColor = _colorBackgroundConfig();
    
    with
    
    backgroundColor = this.backgroundColor != null ? this.backgroundColor : _colorBackgroundConfig();
    
    in line 37

--- lib/src/colorized_text_avatar.dart
+++ lib/src/colorized_text_avatar.dart
@@ -34,7 +34,7 @@ class TextAvatar extends StatelessWidget {
   Widget build(BuildContext context) {
     shape = (shape == null) ? Shape.Rectangle : shape;
     size = (size == null || size! < 32.0) ? 48.0 : size;
-    backgroundColor = _colorBackgroundConfig();
+    backgroundColor = this.backgroundColor != null ? this.backgroundColor : _colorBackgroundConfig();
     textColor = _colorTextConfig();
     return _textDisplay();
   }
